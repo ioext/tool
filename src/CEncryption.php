ade4616f9e45c8655089c1663aeca3f7e20429dc
sadkasadasd <2644218983@qq.com>
2017-12-01 17:26:33 +0800

    init

--- src/CEncryption.php
+++ src/CEncryption.php
@@ -9,7 +9,7 @@ class CEncryption
      *  @link   http://www.w3school.com.cn/php/func_string_md5.asp
      *
      * @param $sTmp
-     * @param bool $bDefault
+     * @param bool $bDefault false 32位, true 15位
      * @param null $sRtn
      * @param string $sDesc
      * @return int
